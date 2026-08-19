4490da1bff7cce563a69b32b31c33bdaa1f692a0
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-19 06:53:25 +0300

    Part 01 —Q1  Object Copying

--- Assignment_5_OOP/Program.cs
+++ Assignment_5_OOP/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 — Theoretical Questions
+            #region Q1  Object Copying
+
+            //QA:
+            //Q:What happens when you assign one object variable to another object variable?
+
+            /*Ans: When we assign one object reference to another,
+             * both references point to the same object in memory. 
+             * No new object or copy of the data is created.
+             */
+
+            //==========================================================
+
+            //QB:
+            //Q:b) Does assigning one object to another create a new object? Explain
+
+            /*Ans:
+             * No not create a new object , both point to same object in memory
+             */
+
+            //==========================================================
+
+            //QC:
+            //Q: What is the difference between copying an object and copying its reference?
+
+            /*Ans:
+             *  In copying an object two object in stack have diffrent address but in heap point to same object
+             *  In copying its reference two variable in stack have same address(copy address) so two variable ref
+                to same data in heap
+             */
+            #endregion
+            #endregion
         }
     }
 }
