2755122a738d954d7fd772a25429f3a50e4864e0
marwanmohameddd2006-ai <marwanmohameddd2006@gmail.com>
2026-08-19 07:30:52 +0300

    Part 01 —Q3  Static Members

--- Assignment_5_OOP/Program.cs
+++ Assignment_5_OOP/Program.cs
@@ -86,6 +86,60 @@
              * Deep Copy changes the copied Address do not affect the original object
              */
             #endregion
+
+            #region Q3  Static Members
+
+            //QA
+            //Q: What is a static field, and how is it different from an instance field?
+
+            /*Ans:
+             * 1- static field belong to the class not to any instance
+             
+             * 2- different is:
+                      static shared by all object 
+                      accessed using class name without creating an object
+             */
+
+            //==========================================================
+
+            //QB:
+            //Q:What is a static method? Can a static method directly access instance members?
+
+            /*Ans:
+             * 1- static member belong to the class not to any instance
+             
+             * 2- Cannot access instance members just can access static field
+             */
+
+            //==============================================================
+
+            //QC:
+            //Q: What is a static constructor, and when is it executed?
+
+
+            /*Ans:
+             * 1- static constructor Used to intilaize static members in class
+                                     Called automatically by CLR , canot called manually
+                                     Cannot take any paramters
+                                     They are always private and cannot be explitly called 
+             
+             * 2- Executed ==> Called one time only [before first use of class]
+             */
+
+            //===================================================================
+
+            //QD:
+            //Q: What is a static class? Can you create an object from a static class? 
+
+
+
+            /*Ans:
+             * 1- A class that cannot be instantiated and can contain only static members.
+             
+             * 2- Cannot create an object from a static class 
+             */
+
+            #endregion
             #endregion
         }
     }
